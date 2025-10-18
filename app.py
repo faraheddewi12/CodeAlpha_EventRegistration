ae714cbf6f4d2c7c1698832036c1aae03a0a4d5d
faraheddewi12 <faraheddewi12@gmail.com>
2025-10-18 21:53:49 +0300

    CodeAlpha Task 2: Event Registration System

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ from flask_sqlalchemy import SQLAlchemy
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///events.db'
 db = SQLAlchemy(app)
-#hhhh
+
 class Event(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(100), nullable=False)
